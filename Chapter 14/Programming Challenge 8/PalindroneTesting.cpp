c682e0b0ff6a11cee5433c592289daa1d7b3ce5a
Blake Blackwell <16102658+PythonPsychonaut@users.noreply.github.com>
2018-04-09 11:32:42 -0400

    Update PalindroneTesting.cpp
    Done

--- Chapter 14/Programming Challenge 8/PalindroneTesting.cpp
+++ Chapter 14/Programming Challenge 8/PalindroneTesting.cpp
@@ -1,3 +1,28 @@
-//Write recursive function
+#include <iostream>
+#include<cstring>
+using namespace std;
 
-bool isPalindrone(string str, int lower, int upper)
+int main(){
+    char string1[20];
+    int i, length;
+    int flag = 0;
+    
+    cout << "This program is used to check if an entered string is a palendrone. Cats is not a palendrone ;)" << endl;
+    cout << "Enter a word to check: "; cin >> string1;
+    
+    length = strlen(string1);
+    
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+   }
+}
+    
+    if (flag) {
+        cout << string1 << " is not a palindrone, a palendrone reads the same forwards and backwards, IE: radar." << endl; 
+    }    
+    else {
+        cout << string1 << " is a palindrone" << endl; 
+    }
+}
